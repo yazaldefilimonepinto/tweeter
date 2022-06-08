6a46b432884d712d507cbae315f605c3df53bd98
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-08 18:13:44 +0200

    feat: init create header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,43 @@
-import { ReactNode } from 'react';
-
 import styles from '@/styles/components/Header/styles.module.scss';
-
-interface HeaderProps {
-  children?: ReactNode;
-}
-
-export function Header({ children }: HeaderProps) {
+import profile from '@/assets/images/profile.jpeg';
+export function Header() {
   return (
-    <header className={styles.container}>
-      <h1>Header</h1>
-      {children}
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <a href="#">tweeter</a>
+        <div>
+          <ul>
+            <li>
+              <a href="#home">
+                <i className="bx bx-home"></i>
+                <span>Home</span>
+              </a>
+            </li>
+
+            <li>
+              <a href="#explore">
+                <i className="bx bx-bolt-circle"></i>
+                <span>Explore</span>
+              </a>
+            </li>
+
+            <li>
+              <a href="#bookmarks">
+                <i className="bx bx-bookmark"></i>
+                <span>BookMarks</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className={styles.profile}>
+          <div className={styles.nav__img}>
+            <img src={profile.src} alt="profile" />
+          </div>
+          <span>Daniel Jensen</span>
+          <i className="bx bx-caret-down"></i>
+        </div>
+      </nav>
     </header>
   );
 }
