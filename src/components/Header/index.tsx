4d417a20004209e4472e6f5dd75bccafd254cc24
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-08 17:13:15 +0200

    feat: create default project

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react';
+
+import styles from '@/styles/components/Header/styles.module.scss';
+
+interface HeaderProps {
+  children: ReactNode;
+}
+
+export function Header({ children }: HeaderProps) {
+  return (
+    <header className={styles.container}>
+      <h1>Header</h1>
+      {children}
+    </header>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app';
-import '@/styles/global.scss';
+import '@/styles/main/global.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
