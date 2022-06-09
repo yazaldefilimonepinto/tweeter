f17e02e591dd315a65837ac73a1149b76f0ce19c
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-09 16:57:25 +0200

    chore: create document tag

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet" />
+        <meta name="description" content="social network" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
