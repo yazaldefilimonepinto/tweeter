26ce4a4ee665bdd5b9bdd486908bb561f778e226
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-10 17:48:20 +0200

    feat: add wrapper of pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import { AppProps } from 'next/app';
 import '@/styles/main/global.scss';
+import Wrapper from '@/components/Wrapper';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Wrapper>
+      <Component {...pageProps} />
+    </Wrapper>
+  );
 }
