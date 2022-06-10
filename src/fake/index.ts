ab68ef3f4ec4bfa5c409e2ec3b4b6fd9fed4031a
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-10 17:47:57 +0200

    feat : create fake datas of api

---
+++ src/fake/index.ts
@@ -0,0 +1,45 @@
+import photo from '@/assets/images/profile.jpeg';
+import banner from '@/assets/images/banner.jpg';
+export const fakeTag = [
+  {
+    name: '#programming',
+    tweet: '213k Tweets',
+  },
+  {
+    name: '#devchallenges',
+    tweet: '123k Tweets',
+  },
+  {
+    name: '#frontend',
+    tweet: '34k Tweets',
+  },
+  {
+    name: '#helsinki',
+    tweet: '11k Tweets',
+  },
+  {
+    name: '#100DaysOfCode',
+    tweet: '5k Tweets',
+  },
+  {
+    name: '#100DaysOfCode',
+    tweet: '1k Tweets',
+  },
+];
+
+export const fakeUser = [
+  {
+    name: 'Mikael Stanley',
+    followers: '230k followers',
+    banner: banner.src,
+    photo: photo.src,
+    description: 'Photographer & Filmmaker based in Copenhagen, Denmark ✵ 🇩🇰',
+  },
+  {
+    name: 'Austin Neill',
+    followers: '120k followers',
+    banner: banner.src,
+    photo: photo.src,
+    description: 'Follow me on IG: @arstyy',
+  },
+];
