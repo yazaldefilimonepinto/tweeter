16be582266764631881091e17496aca3bbcfaa02
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-10 17:49:55 +0200

    feat: create profile page

---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react';
+
+const Profile: FunctionComponent = () => {
+  return (
+    <div>
+      <h2>Profile</h2>
+    </div>
+  );
+};
+export default Profile;
