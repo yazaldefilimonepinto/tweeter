dec86e552a0540047d21a9c56107b008cf78f194
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-12 01:58:36 +0200

    feat: create bookmarks pge

--- src/pages/bookmarks/index.tsx
+++ src/pages/bookmarks/index.tsx
@@ -1,13 +1,37 @@
+import { Container } from '@/components/global/Container';
 import React, { FunctionComponent } from 'react';
-
-//import { Container } from './styles';
-//import styles from './BookMarks.module.css';
-
+import styles from '@/styles/pages/Explore.module.scss';
+import { Button } from '@/components/global/Button';
+import { Tweet } from '@/components/Tweet';
 const BookMarks: FunctionComponent = () => {
+  const bookmarksTag = ['Tweets', 'Tweets & replies', 'Media', 'Likes'];
+
   return (
-    <div>
-      <h2>BookMarks</h2>
-    </div>
+    <Container>
+      <article className={styles.article}>
+        <aside className={styles.aside}>
+          <ul className={styles.asideList}>
+            {bookmarksTag.map((tag, key) => (
+              <li key={key} className={styles.asideChildren}>
+                {tag}
+              </li>
+            ))}
+          </ul>
+        </aside>
+        <section className={styles.content}>
+          <article className={styles.tweets}>
+            <Tweet />
+            <Tweet />
+            <Tweet />
+            <Tweet />
+            <Tweet />
+            <Tweet />
+            <Tweet />
+            <Tweet />
+          </article>
+        </section>
+      </article>
+    </Container>
   );
 };
 export default BookMarks;
