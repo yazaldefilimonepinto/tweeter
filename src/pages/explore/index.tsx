1c5a78b9bdc55957024ea11dc1cffc326360097e
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-12 01:58:06 +0200

    feat: create explore page

--- src/pages/explore/index.tsx
+++ src/pages/explore/index.tsx
@@ -1,13 +1,42 @@
+import { Container } from '@/components/global/Container';
 import React, { FunctionComponent } from 'react';
-
-//import { Container } from './styles';
-//import styles from './Explore.module.css';
+import styles from '@/styles/pages/Explore.module.scss';
+import { Button } from '@/components/global/Button';
+import { Tweet } from '@/components/Tweet';
 
 const Explore: FunctionComponent = () => {
+  const exploreTag = ['Top', 'Lastest', 'People', 'Media'];
   return (
-    <div>
-      <h2>Explore</h2>
-    </div>
+    <Container>
+      <article className={styles.article}>
+        <aside className={styles.aside}>
+          <ul className={styles.asideList}>
+            {exploreTag.map((tag, key) => (
+              <li key={key} className={styles.asideChildren}>
+                {tag}
+              </li>
+            ))}
+          </ul>
+        </aside>
+        <section className={styles.content}>
+          <div className={styles.search}>
+            <i className="bx bx-search"></i>
+            <input type="text" placeholder="Search" />
+            <Button text="Search" />
+          </div>
+          <article className={styles.tweets}>
+            <Tweet />
+            <Tweet />
+            <Tweet />
+            <Tweet />
+            <Tweet />
+            <Tweet />
+            <Tweet />
+            <Tweet />
+          </article>
+        </section>
+      </article>
+    </Container>
   );
 };
 export default Explore;
