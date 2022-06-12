efc82fe426ddb0136b252f09d61614bffac4d39e
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-06-12 22:27:33 +0200

    feat: create modal component

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -3,7 +3,8 @@ import styles from '@/styles/components/Modal/styles.module.scss';
 
 type ModalProps = {
   children: ReactNode;
+  status: boolean;
 };
-export const Modal: FunctionComponent<ModalProps> = ({ children }) => {
-  return <div className={styles.container}>{children}</div>;
+export const Modal: FunctionComponent<ModalProps> = ({ children, status }) => {
+  return <div className={status ? `${styles.container} ${styles.activeModal}` : styles.container}>{children}</div>;
 };
